678a15e47cc947ac2a59fc9f97282bde7cf6e03f
Jancee <jancee800@gmail.com>
2026-04-06 06:36:18 +0800

    fix error at runtime eval

--- src/recognizers/neural_networks/training_loop.py
+++ src/recognizers/neural_networks/training_loop.py
@@ -224,10 +224,11 @@ def get_loss_terms(
             recognition_accuracy,
             num_examples_denominator
         )
-        result['recognition_prediction'] = (
-            recognition_predictions.long(),
-            None
-        )
+        if numerator_reduction == 'none':
+            result['recognition_prediction'] = (
+                recognition_predictions.long(),
+                None
+            )
     if language_modeling_logits is not None:
         # Compute the language modeling loss using cross-entropy.
         pad_index = model_interface.output_padding_index
