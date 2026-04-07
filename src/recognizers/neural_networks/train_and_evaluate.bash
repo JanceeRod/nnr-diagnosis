66e11cb29716802406ffa63667e14f1a462cdc2b
Jancee <jancee800@gmail.com>
2026-04-07 20:03:15 +0800

    set max epoch to 1k

--- src/recognizers/neural_networks/train_and_evaluate.bash
+++ src/recognizers/neural_networks/train_and_evaluate.bash
@@ -88,7 +88,7 @@ python recognizers/neural_networks/train.py \
   "${model_flags[@]}" \
   --init-scale 0.1 \
   "${loss_term_flags[@]}" \
-  --max-epochs 120 \
+  --max-epochs 1000 \
   --max-tokens-per-batch "$(random_sample --int 128 4096)" \
   --optimizer Adam \
   --initial-learning-rate "$(random_sample --log 0.0001 0.01)" \
